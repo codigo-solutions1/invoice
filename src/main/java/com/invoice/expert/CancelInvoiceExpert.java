a9ef9add305699dad7b315049ad321cff24c7c97
Ifra Zaheer <ifrazaheerifrazaheer@gmail.com>
2024-11-28 22:53:05 -0800

    cancel Api call

---
+++ src/main/java/com/invoice/expert/CancelInvoiceExpert.java
@@ -0,0 +1,15 @@
+package com.invoice.expert;
+
+import com.invoice.dto.invoice.CancelInvoiceDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CancelInvoiceExpert {
+
+    public void cancel(CancelInvoiceDTO cancelInvoiceDTO) {
+
+    }
+}
