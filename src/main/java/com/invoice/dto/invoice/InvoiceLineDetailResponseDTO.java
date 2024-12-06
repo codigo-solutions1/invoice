bbf73015648d4068637c3c62b03824eb583c6a4b
Muqit12 <154354866+Muqit12@users.noreply.github.com>
2024-12-06 18:48:41 +0500

    Fixed response and model cahanges in Invoice

--- src/main/java/com/invoice/dto/invoice/InvoiceLineDetailResponseDTO.java
+++ src/main/java/com/invoice/dto/invoice/InvoiceLineDetailResponseDTO.java
@@ -1,13 +1,17 @@
 package com.invoice.dto.invoice;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 public class InvoiceLineDetailResponseDTO {
     private BigDecimal fee;
     private int quantity;
